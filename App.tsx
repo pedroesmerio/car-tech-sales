1fa89cda85c77b2ccbd1caa55c56e56ffbcb52fa
Pedro Esmerio <p.h.esmerio@gmail.com>
2022-08-21 03:51:23 -0300

    Fix: Fixing deprecation os expo-app-loading

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 
-import AppLoading from 'expo-app-loading'
 import { ThemeProvider } from 'styled-components/native';
 import GlobalTheme from '@src/global/styles/theme';
 
 import { StatusBar } from 'expo-status-bar';
 import Home from '@src/screens/Home';
 
+import * as SplashScreen from 'expo-splash-screen';
 import {
   useFonts,
   Roboto_700Bold,
   Roboto_500Medium,
   Roboto_400Regular,
-} from '@expo-google-fonts/roboto'
+} from '@expo-google-fonts/roboto';
 
 export default function App() {
+  SplashScreen.preventAutoHideAsync();
+
   const [fontsLoaded] = useFonts({
     Roboto_700Bold,
     Roboto_500Medium,
@@ -22,8 +24,10 @@ export default function App() {
   })
 
   if (!fontsLoaded) {
-    return <AppLoading />
+    return null;
   }
+
+  SplashScreen.hideAsync();
 
   return (
     <ThemeProvider theme={GlobalTheme}>
