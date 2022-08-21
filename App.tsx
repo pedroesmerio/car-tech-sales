791b3e6b721fb1cdc8b8cabbad07760b482f089f
Pedro Esmerio <p.h.esmerio@gmail.com>
2022-08-20 21:43:57 -0300

    Config: Create font theme using google fonts

--- App.tsx
+++ App.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
+
+import AppLoading from 'expo-app-loading'
 import { ThemeProvider } from 'styled-components/native';
-import { StatusBar } from 'expo-status-bar';
-
-import Dashboard from '@src/screens/Dashboard';
-
 import GlobalTheme from '@src/global/styles/theme';
 
+import { StatusBar } from 'expo-status-bar';
+import Dashboard from '@src/screens/Dashboard';
+
+import {
+  useFonts,
+  Poppins_700Bold,
+  Poppins_500Medium,
+  Poppins_400Regular,
+} from '@expo-google-fonts/poppins'
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_700Bold,
+    Poppins_500Medium,
+    Poppins_400Regular,
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
     <ThemeProvider theme={GlobalTheme}>
       <StatusBar
