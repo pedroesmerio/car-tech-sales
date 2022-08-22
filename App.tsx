bc96f2e4b3b8c123ec303ba58e164ef48d10fb0a
Pedro Esmerio <p.h.esmerio@gmail.com>
2022-08-21 21:36:42 -0300

    Bug: Fixing Bug on font loading

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StyleSheet, View } from 'react-native';
 
 import { ThemeProvider } from 'styled-components/native';
 import GlobalTheme from '@src/global/styles/theme';
@@ -6,35 +7,59 @@ import GlobalTheme from '@src/global/styles/theme';
 import { StatusBar } from 'expo-status-bar';
 import Home from '@src/screens/Home';
 
+import { useState, useEffect, useCallback } from 'react';
 import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
 import {
-  useFonts,
-  Roboto_700Bold,
-  Roboto_500Medium,
   Roboto_400Regular,
-} from '@expo-google-fonts/roboto';
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
 
 export default function App() {
-  SplashScreen.preventAutoHideAsync();
+  const [appIsReady, setAppIsReady] = useState(false)
 
-  const [fontsLoaded] = useFonts({
-    Roboto_700Bold,
-    Roboto_500Medium,
-    Roboto_400Regular,
-  })
+  useEffect(() => {
+    (async () => {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        await Font.loadAsync({
+          Roboto_400Regular,
+          Roboto_500Medium,
+          Roboto_700Bold,
+        });
+      }
+      catch {
+        // handle error
+      }
+      finally {
+        setAppIsReady(true);
+      }
+    })();
+  }, [])
 
-  if (!fontsLoaded) {
+  const onLayout = useCallback(() => {
+    if (appIsReady) {
+      SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
     return null;
   }
 
-  SplashScreen.hideAsync();
-
   return (
-    <ThemeProvider theme={GlobalTheme}>
-      <StatusBar
-        style="inverted"
-      />
-      <Home />
-    </ThemeProvider>
-  )
+    <View onLayout={onLayout} style={styles.container}>
+      <ThemeProvider theme={GlobalTheme}>
+        <StatusBar style="inverted" />
+        <Home />
+      </ThemeProvider>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
