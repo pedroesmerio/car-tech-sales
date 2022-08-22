965d4978e23f0c1cd0450d38bc94bac081bf61da
Pedro Esmerio <p.h.esmerio@gmail.com>
2022-08-22 06:21:18 -0300

    Config: Create a carList file

---
+++ src/utils/carsList.ts
@@ -0,0 +1,52 @@
+export const carList = [
+  {
+    key: '0',
+    name: 'RAV4',
+    brand: 'Toyota',
+    year: 2022,
+    transmission: 'auto',
+    fuel: 'RAV4',
+    type: 'Hatch',
+    basePrice: 230.981
+  },
+  {
+    key: '1',
+    name: 'RAV4',
+    brand: 'Toyota',
+    year: 2022,
+    transmission: 'auto',
+    fuel: 'RAV4',
+    type: 'Hatch',
+    basePrice: 230.981
+  },
+  {
+    key: '2',
+    name: 'RAV4',
+    brand: 'Toyota',
+    year: 2022,
+    transmission: 'auto',
+    fuel: 'RAV4',
+    type: 'Hatch',
+    basePrice: 230.981
+  },
+  {
+    key: '3',
+    name: 'RAV4',
+    brand: 'Toyota',
+    year: 2022,
+    transmission: 'auto',
+    fuel: 'RAV4',
+    type: 'Hatch',
+    basePrice: 230.981
+  },
+  {
+    key: '4',
+    name: 'RAV4',
+    brand: 'Toyota',
+    year: 2022,
+    transmission: 'auto',
+    fuel: 'RAV4',
+    type: 'Hatch',
+    basePrice: 230.981
+  },
+]
