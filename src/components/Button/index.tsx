6a49ff8db53199777748c3f9f857cf0026f6644b
Pedro Esmerio <p.h.esmerio@gmail.com>
2022-08-24 00:57:42 -0300

    Feat: New EditButton

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
-import { Container, Title } from './styles'
+import { Container, EditButtonContainer, Title } from './styles'
 
 type ButtonProps = {
   name: string;
   type: 'add' | 'save';
+}
+
+type EditButtonProps = {
+  name: string;
+  type: 'add' | 'edit';
 }
 
 export function Button({ name, type }: ButtonProps) {
@@ -12,6 +17,14 @@ export function Button({ name, type }: ButtonProps) {
     <Container type={type}>
       <Title>{name}</Title>
     </Container>
+  );
+}
+
+export function EditButton({ name, type }: EditButtonProps) {
+  return (
+    <EditButtonContainer type={type}>
+      <Title>{name}</Title>
+    </EditButtonContainer>
   );
 }
 
